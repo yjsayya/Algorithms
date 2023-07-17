386149db7995c8acd52cae7c66fb073cdfc71871
sayya <djdj9876@naver.com>
2023-07-17 18:27:57 +0900

    PCCP 준비 중 ...

--- 1. Programmers/lv1/lv1(정답률59%이하)/lv1_문자열나누기.py
+++ 1. Programmers/lv1/lv1(정답률59%이하)/lv1_문자열나누기.py
@@ -1,8 +1,7 @@
-
 """
     아이디어 --> 어떻게 풀어야 깔끔할까?
-"""
 
+"""
 
 def solution(s):
     cnt = 0
@@ -25,3 +24,21 @@ def solution(s):
         return cnt
     else:
         return cnt + 1
+
+
+def solution2(s):
+    answer = 0
+    cnt1 = 0
+    cnt2 = 0
+
+    for i in s:
+        if cnt1 == cnt2:
+            answer += 1
+            x = i
+
+        if i == x:
+            cnt1 += 1
+        else:
+            cnt2 += 1
+
+    return answer
