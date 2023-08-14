911d92411814a2bb2de475d45c64b3b97294447a
sayya <djdj9876@naver.com>
2023-08-14 20:51:09 +0900

    //

--- 0. 상황판/prac.py
+++ 0. 상황판/prac.py
@@ -1,67 +1,45 @@
-# import sys, heapq
-#
-# # 1. 입력 받기
-# n = int(input())
-# li = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
-#
-# # 2. 정렬해줌
-# li.sort()
-#
-# # 3. heapq로 정렬
-# queue = []
-# heapq.heappush(queue, li[0][1])
-#
-# for i in range(1,n):
-#     if li[i][0] < queue[0]:
-#         heapq.heappush(queue, li[i][1])
-#     else:
-#         heapq.heappop(queue)
-#         heapq.heappush(queue, li[i][1])
-#
-# print(len(queue))
+import sys
+
+n = int(sys.stdin.readline())
+tree = dict()
+
+for _ in range(n):
+    root, left,right = sys.stdin.readline().rstrip().split()
+    tree[root] = [left,right]
+
+print(tree)
+
+def preorder(root):
+    # 1. 종료 조건
+    if root == '.':
+        return
+    # 2. 문제에 대한 정의
+    else:
+        print(root, end='')
+        preorder(left)
+        preorder(right)
 
 
-# import sys,heapq
-#
-# n = int(sys.stdin.readline())
-# li = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
-#
-# li.sort(key= lambda x : x[0])
-#
-# q = []
-# heapq.heappush(q, li[0][1])
-#
-# for i in range(1,n):
-#     if li[i][0] < q[0]:
-#         heapq.heappush(q,li[i][1])
-#     else:
-#         heapq.heappop(q)
-#         heapq.heappush(q,li[i][1])
-#
-# print(len(q))
+def inorder(root):
+    # 1. 종료 조건
+    if root == '.':
+        return
+    # 2. 문제에 대한 정의
+    else:
+        preorder(left)
+        print(root, end='')
+        preorder(right)
 
-# import heapq
-# def solution(book_time):
-#     cnt = 0
-#     hq = []
-#     heapq.heapify(hq)
-#
-#     # 1. 정렬하기
-#     book_time.sort(key=lambda x : (x[0][:2], x[0][3:]))
-#
-#     # 2. heapq에 값 넣기
-#     heapq.heappush(hq,int())
-#
-#
-#
-# print(solution([["15:00", "17:00"], ["16:40", "18:20"], ["14:20", "15:20"], ["14:10", "19:20"], ["18:20", "21:20"]]))
-#
-# def plusTenMin(hr,min):
-#     if 0 <= min <= 49:
-#         return str(hr) + ':' + str(min+10)
-#     else:
-#         strMin = '0' + str((min+10) % 60)
-#         if hr == 23:
-#             return '00:' + strMin
-#         else:
-#             return str(hr+1) + ':' + strMin
+def postorder(root):
+    # 1. 종료 조건
+    if root == '.':
+        return
+    # 2. 문제에 대한 정의
+    else:
+        preorder(left)
+        preorder(right)
+        print(root, end='')
+
+preorder('A')
+inorder('A')
+postorder('A')
---
+++ 2. boj/재귀/g5_2447_별찍기10.py
@@ -0,0 +1,22 @@
+# """"""
+# """
+#
+# """
+# import sys
+# sys.setrecursionlimit(10**6)
+#
+# def printStar(len):
+#     div3 = len//3
+#     if len == 3:
+#         g[1] = ['*', ' ', '*']
+#         g[0][:3] = g[2][:3] = ['*'] * 3
+#         return
+#     printStar(div3)
+#
+#     for i in range()
+#
+# n = int(sys.stdin.readline())
+#
+# def recur(n):
+#     # 1. 종료 조건
+#     print("*")
