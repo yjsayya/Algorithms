e49c0b5829b2dca5b39414743448ece7fb4d0512
sayya <djdj9876@naver.com>
2023-08-28 18:15:04 +0900

    dfs

---
+++ 2. boj/BFS_DFS/DFS_BFS모음집/g5_7569_토마토.py
@@ -0,0 +1,14 @@
+""""""
+"""
+
+"""
+
+import sys
+
+m,n,h = map(int,sys.stdin.readline().split())
+
+tomato = []
+for _ in range(n):
+    tomato.append(list(map(int,sys.stdin.readline().split())))
+
+print(tomato)
